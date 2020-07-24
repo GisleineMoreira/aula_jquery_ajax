277c1d0c0bdb6a23c435c7608d45146e34998104
Gisleine <gisleine.moreira@gmail.com>
2020-07-24 17:02:35 -0300

    add botstrap e  criei tabela

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 //alert("funcionou");
 
 function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" +  cep + "/json/";
+    
+    console.log(url);
    //$.ajax é uma função do jquery
     $.ajax({
-        url: "https://viacep.com.br/ws/71593385/json/",
+        url: url,
         type: "GET",
         success: function(response){
             console.log(response);
+            $("#logradouro").html(response.logradouro);
+           $("#uf").html(response.uf);
+           $("#localidade").html(response.localidade);
+           $("#bairro").html(response.bairro);
+           $("#titulo_cep").html("cep " + response.cep);
         }
     })
 }
